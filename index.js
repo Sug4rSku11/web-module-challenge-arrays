a0ed2875c66ceb8ec79a0ebfe31fb7b28a843432
desiree <desireegarcia@mini.local>
2021-09-08 14:12:17 -0600

    worked on task one through 4

--- index.js
+++ index.js
@@ -44,10 +44,11 @@ Use the copy function below to do the following:
   1. receive an array as a parameter - you will pass in originalFlavors as an argument when the function is invoked.
   2. Return a copy of the received array  
 */
-
-function copy(/*your code here*/){
-  /*your code here*/
-}    
+const flavors = originalFlavors;
+function copy(flavors){
+  return flavors;
+}   
+console.log(flavors);
 
 
 
@@ -64,9 +65,13 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
-}
+function is31Flavors(originalFlavors){
+  if (originalFlavors.length === 31) {
+    return true;
+  } else {
+    return false;
+  }
+} 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,8 +86,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(originalFlavors){
+ originalFlavors.unshift('Rainbow Sherbert');
+ return originalFlavors;
 }
 
 
@@ -97,8 +103,9 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(originalFlavors){
+ originalFlavors.pop();
+ return originalFlavors;
 }
 
 
